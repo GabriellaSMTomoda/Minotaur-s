9cf8b1204881b6ffa3c1aa96255423071ccd8c3f
GabriellaSMTomoda <gabriellasmt@gmail.com>
2025-08-18 10:24:03 -0300

    Corrige arquivos

---
+++ Minotaur-s/Minotaur-s/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Minotaur-s
+//
+//  Created by Gabriella San Martino Tomoda on 18/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
