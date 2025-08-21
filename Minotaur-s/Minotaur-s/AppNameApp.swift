2ce1cd8f2c16e741a5a67670bc24e40d3791639b
GabriellaSMTomoda <gabriellasmt@gmail.com>
2025-08-21 10:37:52 -0300

    retira arquivo duplicado de BD

---
+++ Minotaur-s/Minotaur-s/AppNameApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppNameApp.swift
+//  Minotaur-s
+//
+//  Created by Gabriella San Martino Tomoda on 21/08/25.
+//
+
+
+import SwiftUI
+
+@main
+struct AppNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
