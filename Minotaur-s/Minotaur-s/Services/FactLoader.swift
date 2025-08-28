0a52bdbb6d2f0ce30a36147129268e826a0fa62c
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-08-28 09:33:15 -0300

    design tool bar

--- Minotaur-s/Minotaur-s/Services/FactLoader.swift
+++ Minotaur-s/Minotaur-s/Services/FactLoader.swift
@@ -1,41 +1,35 @@
 //
-//  FactLoader.swift
-//  Minotaur-s
+// FactLoader.swift
+// Minotaur-s
 //
-//  Created by Gabriella San Martino Tomoda on 19/08/25.
+// Created by Gabriella San Martino Tomoda on 19/08/25.
 //
-
 import Foundation
-
 class FactLoader {
-    static func loadFacts() -> [Fact] {
-        guard let url = Bundle.main.url(forResource: "BancoDeDados", withExtension: "tsv"),
-              let content = try? String(contentsOf: url) else {
-            return []
-        }
-        
-        var facts: [Fact] = []
-        let lines = content.components(separatedBy: "\n")
-        
-        // pular a primeira linha (header)
-        for line in lines.dropFirst() {
-            let columns = line.components(separatedBy: "\t")
-            if columns.count >= 8 {
-                let fact = Fact(
-                    url: columns[1],
-                    autor: columns[2],
-                    data: columns[3],
-                    motivo: columns[4],
-                    resumo: columns[5],
-                    titulo: columns[6],
-                    binario: columns[7],
-                    assunto: columns[8],
-                )
-                facts.append(fact)
-            }
-        }
-        return facts
+  static func loadFacts() -> [Fact] {
+    guard let url = Bundle.main.url(forResource: "BancoDeDados", withExtension: "tsv"),
+       let content = try? String(contentsOf: url, encoding: .utf8) else {
+      return []
     }
+    var facts: [Fact] = []
+    let lines = content.components(separatedBy: "\n")
+    // pular a primeira linha (header)
+    for line in lines.dropFirst() {
+      let columns = line.components(separatedBy: "\t")
+      if columns.count >= 9 {
+        let fact = Fact(
+          url: columns[1],
+          autor: columns[2],
+          data: columns[3],
+          motivo: columns[4],
+          resumo: columns[5],
+          titulo: columns[6],
+          binario: columns[7],
+          assunto: columns[8]
+        )
+        facts.append(fact)
+      }
+    }
+    return facts
+  }
 }
-
-
