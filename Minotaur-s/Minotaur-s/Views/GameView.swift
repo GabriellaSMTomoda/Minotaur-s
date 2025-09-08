691fb88e7132b5a8a49fe5ac225c2a16295c56d6
ErikaHacimoto <erika.t.s.hacimoto@gmail.com>
2025-09-08 12:09:53 -0300

    VoiceOver

--- Minotaur-s/Minotaur-s/Views/GameView.swift
+++ Minotaur-s/Minotaur-s/Views/GameView.swift
@@ -37,6 +37,8 @@ struct GameView: View {
                                             .cornerRadius(12)
                                             .frame(maxWidth: .infinity, alignment: .leading)
                                             .padding(.bottom, 4)
+                                            .accessibilityLabel("Assunto: \(fact.assunto).")
+                                            .accessibilitySortPriority(1)
                                     }
                                     Text(fact.titulo)
                                         .font(.title.bold())
@@ -45,16 +47,22 @@ struct GameView: View {
                                         .frame(maxWidth: .infinity, alignment: .leading)
                                         .padding(.horizontal)
                                         .padding(.bottom)
+                                        .accessibilityLabel("Título: \(fact.titulo).")
+                                        .accessibilitySortPriority(0)
                                     Text("Fonte: \(fact.fonte)")
                                         .frame(maxWidth: .infinity, alignment: .leading)
                                         .foregroundColor(Color("texto"))
                                         .font(.callout)
                                         .padding(.horizontal)
+                                        .accessibilityLabel("Fonte: \(fact.fonte).")
+                                        .accessibilitySortPriority(3)
                                     Text("Autor: \(fact.autor)")
                                         .frame(maxWidth: .infinity, alignment: .leading)
                                         .foregroundColor(Color("texto"))
                                         .font(.callout)
                                         .padding(.horizontal)
+                                        .accessibilityLabel("Autor: \(fact.autor).")
+                                        .accessibilitySortPriority(4)
                                     Text(fact.resumo)
                                         .multilineTextAlignment(.leading)
                                         .foregroundColor(Color("texto"))
@@ -62,9 +70,10 @@ struct GameView: View {
                                         .font(.title2)
                                         .padding(.horizontal)
                                         .padding(.bottom)
+                                        .accessibilityLabel("Resumo: \(fact.resumo).")
+                                        .accessibilitySortPriority(2)
                                 }
                             }
-                            .frame(maxWidth: .infinity)
                         }
                         Spacer()
                         // Botões de resposta
@@ -80,6 +89,9 @@ struct GameView: View {
                             .frame(minWidth: 160, maxHeight: 64)
                             .background(Color("azul"))
                             .cornerRadius(20)
+                            .accessibilityLabel("FATO")
+                            .accessibilityHint("Toque para confirmar que a notícia é verdadeira.")
+                            .accessibilitySortPriority(5)
                             Button("FARSA") {
                                 checkAnswer(userSaysTrue: false)
                             }
@@ -89,6 +101,9 @@ struct GameView: View {
                             .frame(minWidth: 160, maxHeight: 64)
                             .background(Color("vermelho"))
                             .cornerRadius(20)
+                            .accessibilityLabel("FARSA")
+                            .accessibilityHint("Toque para confirmar que a notícia NÃO é verdadeira.")
+                            .accessibilitySortPriority(6)
                         }
                         .padding(.bottom, 2)
                         Button("Pular") {
@@ -98,6 +113,9 @@ struct GameView: View {
                         .padding(.bottom, -20)
                         .bold()
                         .foregroundColor(Color("cinza"))
+                        .accessibilityLabel("PULAR")
+                        .accessibilityHint("Toque para pular a notícia.")
+                        .accessibilitySortPriority(7)
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .padding()
@@ -144,11 +162,15 @@ struct GameView: View {
                             .frame(height: 55)
                             .background(respostaCorreta ? Color("verde") : Color("vermelho"))
                             .foregroundColor(Color.white)
+                            .accessibilityLabel(respostaCorreta ? "ACERTOU" : "ERROU")
+                            .accessibilitySortPriority(8)
                         ScrollView{
                             if let fact = gameState.factAtual {
                                 Text(fact.motivo)
                                     .foregroundColor(Color("texto"))
                                     .padding(.horizontal)
+                                    .accessibilityLabel("Explicação do motivo: \(fact.motivo)")
+                                    .accessibilitySortPriority(9)
                             }
                         }
                         Button("PRÓXIMA") {
@@ -161,6 +183,9 @@ struct GameView: View {
                         .bold()
                         .foregroundColor(Color.white)
                         .padding(.bottom)
+                        .accessibilityLabel("Botão PRÓXIMA")
+                        .accessibilityHint("Aperte para ver a próxima pergunta")
+                        .accessibilitySortPriority(10)
                     }
                     .frame(width: UIScreen.main.bounds.width * 4/5,
                            height: UIScreen.main.bounds.height * 2/3)
@@ -180,6 +205,8 @@ struct GameView: View {
                             .frame(height: 64)
                             .background(Color("azul"))
                             .foregroundColor(Color.white)
+                            .accessibilityLabel("Fim da Partida")
+                            .accessibilitySortPriority(11)
                         NavigationLink(destination: GameReportView().environmentObject(gameState)) {
                             Text("REVISÃO")
                                 .font(.title2)
@@ -188,6 +215,9 @@ struct GameView: View {
                                 .background(Color("azul"))
                                 .foregroundColor(.white)
                                 .cornerRadius(12)
+                                .accessibilityLabel("Botão REVISÃO")
+                                .accessibilityHint("Aperte para ver os erros e acertos desta partida")
+                                .accessibilitySortPriority(12)
                         }
                         .padding(.horizontal)
                         Button("JOGAR DE NOVO") {
@@ -203,6 +233,9 @@ struct GameView: View {
                         .cornerRadius(12)
                         .padding(.horizontal)
                         .padding(.bottom)
+                        .accessibilityLabel("Botão JOGAR DE NOVO")
+                        .accessibilityHint("Aperte para Jogar um novo jogo")
+                        .accessibilitySortPriority(13)
                     }
                     .frame(width: UIScreen.main.bounds.width * 5/6)
                     .background(Color("pop"))
@@ -223,18 +256,18 @@ struct GameView: View {
             }
             .navigationTitle("\(gameState.currentIndex + 1)/\(gameState.partidaFacts.count)")
             .navigationBarTitleDisplayMode(.inline)
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    Button(action: {
-//                        popupAppIntent = true
-//                    }) {
-//                        Image(systemName: "ellipsis.bubble")
-//                        //.frame(width: 40, height: 40)
-//                            .foregroundColor(.white) // cor do ícone
-//                    }
-//                    //.bold()
-//                }
-//            }
+            //            .toolbar {
+            //                ToolbarItem(placement: .navigationBarTrailing) {
+            //                    Button(action: {
+            //                        popupAppIntent = true
+            //                    }) {
+            //                        Image(systemName: "ellipsis.bubble")
+            //                        //.frame(width: 40, height: 40)
+            //                            .foregroundColor(.white) // cor do ícone
+            //                    }
+            //                    //.bold()
+            //                }
+            //            }
         }
     }
     // Verifica se o usuário acertou ou errou
