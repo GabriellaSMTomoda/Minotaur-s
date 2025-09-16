5b1c38ef0ca75121e79c9c2f664a9cd381729d04
GabriellaSMTomoda <gabriellasmt@gmail.com>
2025-09-16 09:42:27 -0300

    limita intent e adc novas frases
    limita o intent a 1 noticia por dia e aumenta as frases do intent

--- Minotaur-s/Minotaur-s/Services/PerguntaStorage.swift
+++ Minotaur-s/Minotaur-s/Services/PerguntaStorage.swift
@@ -11,23 +11,23 @@ class PerguntaStorage {
     static let shared = PerguntaStorage()
     private let key = "ultimaPergunta"
 
-    func salvarPergunta(_ question: Question) {
+    func salvarPergunta(_ question: Question, data: String) {
         let dict: [String: String] = [
             "titulo": question.titulo,
             "fonte": question.fonte,
             "binario": question.binario,
             "motivo": question.motivo,
             "autor": question.autor,
-            "link": question.link
+            "link": question.link,
+            "data": data
         ]
         UserDefaults.standard.set(dict, forKey: key)
     }
 
-    func carregarPergunta() -> Question? {
-        guard let dict = UserDefaults.standard.dictionary(forKey: key) as? [String: String] else {
-            return nil
-        }
-        return Question(
+    func carregarPergunta() -> (question: Question, data: String)? {
+        guard let dict = UserDefaults.standard.dictionary(forKey: key) as? [String: String],
+              let data = dict["data"] else { return nil }
+        let question = Question(
             titulo: dict["titulo"] ?? "",
             fonte: dict["fonte"] ?? "",
             binario: dict["binario"] ?? "",
@@ -35,5 +35,6 @@ class PerguntaStorage {
             autor: dict["autor"] ?? "",
             link: dict["link"] ?? ""
         )
+        return (question, data)
     }
 }
